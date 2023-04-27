1efd42bb4d4ffb7344b3271724f4012ac1e89773
付昊飞 <haofei.fu@71360.com>
2023-04-27 18:53:27 +0800

    test .vue 类型

--- play/src/App.vue
+++ play/src/App.vue
@@ -1,8 +1,9 @@
 <script setup lang="ts">
+// import Icon from '../../../packages/components/icon/src/icon.vue'
 </script>
 
 <template>
-  <el-icon size="20" />
+  <el-icon size="30" />
 </template>
 
 <style scoped>
