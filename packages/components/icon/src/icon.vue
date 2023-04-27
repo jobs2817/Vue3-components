d5a9a2ea42b62360801b6c04d2311b3534121faa
付昊飞 <haofei.fu@71360.com>
2023-04-27 10:40:35 +0800

    add eslint format

--- packages/components/icon/src/icon.vue
+++ packages/components/icon/src/icon.vue
@@ -1,11 +1,11 @@
-<template>
-  <i>{{ props.color  }}</i>
-</template>
-
 <script setup lang="ts">
 import { computed } from 'vue'
+
 import { iconProps } from './icon'
 
 const props = defineProps(iconProps)
-
 </script>
+
+<template>
+  <i>{{ props.color }}</i>
+</template>
